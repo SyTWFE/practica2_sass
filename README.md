849eb232c093c399550a99a16388503cf2457c69
Felipe Valencia <alu0101283197@ull.edu.es>
2020-08-28 15:16:04 +0100

    Primer commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # practica2_sass
+
+Pre requisitos
+
+```
+$ sudo apt-get install ruby
+$ sudo gem install sass
+```
+
+```
+$ sudo npm install gulp --save-dev
+$ sudo npm install browser-sync --save-dev
+$ sudo npm install gulp-sass --save-dev
+$ sudo npm install dell --save-dev
+```
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+browsersync = require('browser-sync').create(),
+sass = require('gulp-sass'),
+del = require('del');
+
+gulp.task('serve', () =>
+  browsersync.init({
+      server: "./",
+      port: 8070,
+      baseDir: "./"
+  }),
+  gulp.watch("*.html").on('change', browsersync.reload)
+);
+
+gulp.task('sass', () => {
+    return gulp.src('src/styles/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/styles/css'));
+});
+
+gulp.task('del', () => {
+    return del([
+        'src/styles/css/**/*.css'
+    ]);
+});
+
+gulp.task('watch-files', () => {
+    gulp.watch('./sass/*.scss', gulp.series(['del','styles', browsersync.reload]));
+});
+
+gulp.task('default', gulp.series(['del', 'sass', 'serve']));
